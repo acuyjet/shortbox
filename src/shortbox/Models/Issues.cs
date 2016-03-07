21d8e704b0bf69231eeec00c84d74eb63467671f
Allison Cuyjet <allison@cuyjet.com>
2016-03-07 15:36:04 -0500

    Initial commit

---
+++ src/shortbox/Models/Issues.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shortbox.Models
+{
+    public class Issues
+    {
+        [ScaffoldColumn(false)]
+        public int IssueId { get; set; }
+
+        [Required]
+        [Display(Name = "Series")]
+        public string SeriesName { get; set; }
+
+        [Required]
+        [Display(Name = "Issue #")]
+        public string IssueNumber { get; set; }
+
+        public string Publisher { get; set; }
+
+        [Display(Name="Story Arc")]
+        public string StoryArc { get; set; }
+        
+        public string Writer { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Inker { get; set; }
+
+        public string Colorist { get; set; }
+
+        public string Letterer { get; set; }
+
+        //Navigation property
+        public virtual Series Series { get; set; }
+
+    }
+}
