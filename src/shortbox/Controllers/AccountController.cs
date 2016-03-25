e7010ecddf05689359ef86fb87a8be964fbf6f11
Allison Cuyjet <allison@cuyjet.com>
2016-03-24 22:39:52 -0400

    Added redirect after account creation

--- src/shortbox/Controllers/AccountController.cs
+++ src/shortbox/Controllers/AccountController.cs
@@ -117,7 +117,8 @@ namespace shortbox.Controllers
                     //    "Please confirm your account by clicking this link: <a href=\"" + callbackUrl + "\">link</a>");
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation(3, "User created a new account with password.");
-                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                    //return RedirectToAction(nameof(HomeController.Index), "Home");
+                    return Redirect("../Issues");
                 }
                 AddErrors(result);
             }
